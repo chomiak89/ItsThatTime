613e54fa82cb95399fcf4e9602a7ead219ef069d
Kamil Chomiak <kamil.chomiak@outlook.com>
2022-09-11 02:38:39 -0400

    fix port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const mongoose = require("mongoose");
 const flash = require("connect-flash");
 const session = require("express-session");
